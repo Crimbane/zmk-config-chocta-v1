32f22ef7b19ab3930d5799e21aedf3892e6daec5
Crimbane <said-h95@hotmail.com>
2024-08-18 15:28:50 +0200

    Testing Layer Behaviour

---
+++ config/boards/shields/chocta_v1/keymap_norwegian.h
@@ -0,0 +1,40 @@
+#define N_PIP GRAVE     // |
+#define N_PLS MINUS     // +
+#define N_BSL EQUAL     // (backslash)
+#define N_AA  LBKT      // Å
+#define N_DIA RBKT      // ¨ (dead)
+#define N_OO  SEMI      // Ø
+#define N_AE  SQT       // Æ
+#define N_SQT NUHS      // '
+#define N_LT  NUBS      // <
+#define N_MNS FSLH      // -
+#define N_SEC LS(N_PIP) // §
+#define N_EXC LS(N1)    // !
+#define N_DQT LS(N2)    // "
+#define N_HAS LS(N3)    // #
+#define N_CUR LS(N4)    // ¤
+#define N_PRC LS(N5)    // %
+#define N_AMP LS(N6)    // &
+#define N_FSL LS(N7)    // /
+#define N_LPR LS(N8)    // (
+#define N_RPR LS(N9)    // )
+#define N_EQL LS(N0)    // =
+#define N_QUE LS(N_PLS) // ?
+#define N_GRV LS(N_BSL) // ` (dead)
+#define N_CAR LS(N_DIA) // ^ (dead)
+#define N_AST LS(N_SQT) // *
+#define N_GT  LS(N_LT)  // >
+#define N_SCL LS(COMMA) // ;
+#define N_COL LS(DOT)   // :
+#define N_UNS LS(N_MNS) // _
+#define N_AT  RA(N2)    // @
+#define N_PND RA(N3)    // £
+#define N_DLR RA(N4)    // $
+#define N_EUR RA(N5)    // €
+#define N_LCB RA(N7)    // {
+#define N_LSB RA(N8)    // [
+#define N_RSB RA(N9)    // ]
+#define N_RCB RA(N0)    // }
+#define N_ACT RA(N_BSL) // ´ (dead)
+#define N_TLD RA(N_DIA) // ~ (dead)
+#define N_MCR RA(M)     // µ
